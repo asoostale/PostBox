d97b1df976e86e55e47f1a53139292ae286f5aca
SooYoung Joo <asoostale@gmail.com>
2024-03-08 19:00:32 +0900

    main 추가

---
+++ src/main/java/com/postbox/TempController.java
@@ -0,0 +1,25 @@
+package com.postbox;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TempController {
+
+    @GetMapping("/")
+    public String mainForm() {
+        return "ui/ui-page";
+    }
+
+
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "/service/login";
+    }
+
+    @GetMapping("/join")
+    public String joinForm() {
+        return "/service/join";
+    }
+}
--- src/main/java/com/postbox/domain/user/UserController.java
+++ src/main/java/com/postbox/domain/user/UserController.java
@@ -6,14 +6,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-    @GetMapping("/")
-    public String serviceMainForm() {
-        return "fragment/bodyHeader";
-    }
 
-    @GetMapping("/join")
-    public String joinForm() {
-        return "service/join";
-
-    }
 }
