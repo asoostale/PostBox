629f42cddc620845b4a90fa3d49117c7259e871f
SooYoung Joo <asoostale@gmail.com>
2024-04-03 14:38:35 +0900

    댓글 user 저장

--- src/main/java/com/postbox/domain/post/reply/ReplyService.java
+++ src/main/java/com/postbox/domain/post/reply/ReplyService.java
@@ -2,7 +2,11 @@ package com.postbox.domain.post.reply;
 
 import com.postbox.domain.post.Post;
 import com.postbox.domain.post.PostRepository;
+import com.postbox.domain.user.User;
+import com.postbox.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,13 +20,16 @@ public class ReplyService {
 
     private final ReplyRepository replyRepository;
     private final PostRepository postRepository;
-
+    private final UserRepository userRepository;
 
     @Transactional
     public void saveReply(ReplyForm replyForm, Long id){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User findUser = userRepository.findByUsername(username);
         Post findPost = postRepository.findById(id).get();
         Reply reply = new Reply();
         reply.setContents(replyForm.getContents());
+        reply.setUser(findUser);
         reply.setWriteAt(LocalDateTime.now());
         reply.setPost(findPost);
 
