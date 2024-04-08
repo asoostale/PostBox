f9f94213b3ae168cc97e6b6b8c328edc48acf7e3
SooYoung Joo <asoostale@gmail.com>
2024-04-08 15:04:27 +0900

    queryDsl 추가

--- src/main/java/com/postbox/domain/post/PostController.java
+++ src/main/java/com/postbox/domain/post/PostController.java
@@ -12,9 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
