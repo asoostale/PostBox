5ff438196b77b3dddc4b98847197a41c2fbcb4b3
SooYoung Joo <asoostale@gmail.com>
2024-05-28 01:12:27 +0900

    ubuntu

---
+++ src/main/resources/static/src/type2.js
@@ -0,0 +1,14 @@
+'use strict';
+
+new TypeIt('.typeItText2', {
+    loop: true,
+    speed: 100
+})
+    .pause(500)
+    .delete()
+    .pause(500)
+    .type('<span class="highlight">POSTBOX</span>')
+    .pause(5000)
+    .go();
+
+// POSTBOX에 오신 면접관님들 환영합니다. 많이 부족하지만 꾸준히 공부하는 개발자가 되겠습니다. 모두 건강 챙기시고 행복하세요!🤗🤗🤗
